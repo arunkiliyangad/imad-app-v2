3afa7c302dafa6bf0f328abba403690335c6fca7
arunkiliyangad <arunkiliyangad@gmail.com>
2017-03-08 08:32:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,21 @@ var counter=0;
 
 button.onclick=function() {
     // make a request to the cunter endpoint
-    
+        var request=new XMLHttpRequest();
     //capture the responce and store it in a variable
+        request.onreadystatechange=function() {
+            if(request.readyState===XMLHttpRequest.DONE) {
+                //Take some action
+                if(request.status===200) {
+                    var counter=request.responceText;
+                    var span=document.getElementById('count');
+                    span.innerHTML=counter.toString();
+                }
+            }
+            //Not done yet
+        };
+    // Make the request
+        request.open('GET','http://arunkiliyangad.imad.hasura-app.io/counter',true);
+        request.send(null);
     
-    // Render the variable in the correct span
-    
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
 };
