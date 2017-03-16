4527be6125b971922f4e12ac8ed85cd9bf745f2a
arunkiliyangad <arunkiliyangad@gmail.com>
2017-03-16 06:42:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
 
+config={
+    user: 'arunkiliyangad',
+    database: 'arunkiliyangad',
+    host: 'db.imad.hasura.app.io',
+    port: '5432',
+    password: process.env.DB-PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 var articles= {
@@ -78,6 +86,19 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool=new Pool(config);
+app.get('/test-db',function(req,res) {
+    //make a select request
+    //return a responce with result
+    pool.query('SELECT * FROM test', function(err,result){
+        if(err){
+            res.status(500).send(err,toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 var counter=0;
 app.get('/counter', function (req, res) {
