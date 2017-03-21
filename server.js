706eba411af2f2d88a131bed52ebb1572624c252
arunkiliyangad <arunkiliyangad@gmail.com>
2017-03-21 23:11:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
     database: 'arunkiliyangad',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: process.env.db-arunkiliyangad-87588
 };
 var app = express();
 app.use(morgan('combined'));
